658e68eb199d2a643e883b658258cb8e71376511
ganeshkumar <ganeshkumaremandi@gmail.com>
2026-05-07 09:56:07 +0530

    Reorganize and clean up gpt.py code structure

--- gpt.py
+++ gpt.py
@@ -22,12 +22,11 @@ if file is not None:
     splitter=RecursiveCharacterTextSplitter(chunk_size=300,chunk_overlap=50,length_function=len)
     chunks=splitter.split_text(text)
     st.write(chunks)
-    embedding=OpenAIEmbeddings(api_key="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")
-    vector_store=FAISS.from_texts(chunks,embedding)
+    embedding=OpenAIEmbeddings(```python OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") ``` 
     userquery=st.text_input("Type your query here")
     if userquery:
         matching_chunks=vector_store.similarity_search(userquery)
-        llm=ChatOpenAI(api_key="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",max_tokens=300,temperature=1,model="gpt-3.5-turbo")
+        llm=ChatOpenAI(api_key=```python OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") ``` 
         customized_prompt=ChatPromptTemplate.from_template("""You are my assistant tutor.Answer the question based on the following content and if you did not get the context simply say "I don't know jenny:
         {context}
         Question:{input}
